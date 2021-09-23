b92cf79f0ab2e8b517e2b1a584fff7c655e96d4a
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2021-09-23 13:27:39 +0500

    compete Redux implementation for basic flow of app

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const PORT = 8000;
 var dbconnection = require('./db')
 const ProductRoute = require('./routes/ProductsRoute')
+const bodyparser = require('body-parser')
 
+app.use(bodyparser.json());
 app.use('/api/products', ProductRoute)
 
 app.get('/',(req, res)=>{
